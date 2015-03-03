39de7f7fa2c537c0995610180edb1a05936095e0
bwl <benlundquist@gmail.com>
2015-03-02 22:14:20 -0500

    sanity reformatting

--- js/action.js
+++ js/action.js
@@ -1,50 +1,36 @@
-$(document).ready(function() {
-			
-		var titleoriginalTitleText = $("#title").html();
-		var baconoriginalTitleText = $("#bacon-title").html();
-		var nutsBoltsoriginalTitleText = $("#nuts-title").html();
-		
-		
-        $("#no-apples").toggle(function() {
-			
+$().ready(function() {
+
+	$(".tabcontrol").tabs("#entries > div", {
+		effect:'fade',
+		fadeInSpeed:'4000'
+	});
+
+	var titleoriginalTitleText = $("#title").html();
+	var baconoriginalTitleText = $("#bacon-title").html();
+	var nutsBoltsoriginalTitleText = $("#nuts-title").html();
+
+	$("#no-apples").toggle(function() {
 			$("#title").html("∴ NO FAIRS!!!");
 			pageTracker._trackPageview("apple-nofair");
-
-
-
-        	}, function() {
-
+	}, function() {
 			$("#title").html(titleoriginalTitleText);
 			pageTracker._trackPageview("apple-noapples");
-			
-        });
+  });
 
-
-        $("#bacon-date").toggle(function() {
-			
+	$("#bacon-date").toggle(function() {
 			$("#bacon-title").html("Don't HATE!!");
 			pageTracker._trackPageview("bacon-dont-hate");
-
-        	}, function() {
-
+	}, function() {
 			$("#bacon-title").html(baconoriginalTitleText);
 			pageTracker._trackPageview("bacon-dates");
-			
-        });
+  });
 
-
-        $("#nuts-bolts").toggle(function() {
-			
+  $("#nuts-bolts").toggle(function() {
 			$("#nuts-title").html("Some are about fasteners!!!");
 			pageTracker._trackPageview("fasteners");
-
         	}, function() {
-
-			$("#nuts-title").html(nutsBoltsoriginalTitleText);
+	$("#nuts-title").html(nutsBoltsoriginalTitleText);
 			pageTracker._trackPageview("jokesaboutfood");
-			
-        });
+  });
 
-
-
-});	
+});
